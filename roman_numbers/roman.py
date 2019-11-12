a43113166cc267ebd5263ba3d15859f1cdf57723
Paula Onea <paula.onea@gmail.com>
2019-11-12 13:08:09 +0000

    small changes

--- roman_numbers/roman.py
+++ roman_numbers/roman.py
@@ -1,9 +1,12 @@
 
 def convert_from_roman(value):
+    # Go through Roman number letter by letter starting from the end
+    # If current letter is smaller in value than the previous letter, the final value decreases (IV, IX, etc)
+    # If current letter is larger in value than the previous letter, the final value increases
+
     roman_letters = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     n = len(value)
     number = roman_letters[value[-1]]
-
     if len(value) > 1:
         for i in range(2, n+1):
             if roman_letters[value[n-i]] < number:
@@ -20,9 +23,7 @@ def convert_to_roman(number):
                      'X': 10, 'IX': 9,
                      'V': 5, 'IV': 4, 'I': 1
                      }
-
     roman_number = ''
-
     for key, val in roman_letters.items():
         while number >= val:
             roman_number += key
