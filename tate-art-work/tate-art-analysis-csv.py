718dd2b27e7578e76445f8a0a45a9e8b6f5ae375
Paula Onea <paula.onea@gmail.com>
2020-01-09 15:15:15 +0000

    some analysis of the database

--- tate-art-work/tate-art-analysis-csv.py
+++ tate-art-work/tate-art-analysis-csv.py
@@ -16,5 +16,23 @@ csv_path = os.getcwd() + '/collection-master/artwork_data.csv'
 df = read_artwork_from_csv(csv_path)
 
 
+# Some analysis:
+
+print('\nSome analysis: \n')
+
+# number of unique artists in the data
+artists = df['artist']
+unique_artists = pd.unique(artists)
+print(f'There are {len(unique_artists)} unique artists in the database.\n')
+
+# Number of artworks by a specific artist
+artist_counts = df['artist'].value_counts()
+print(f"Artist Francis Bacon has {artist_counts['Bacon, Francis']} artworks in the database. \n")
+
+# Find the largest artwork (by area)
+df.loc[:, 'height'] = pd.to_numeric(df['height'], errors='coerce')
+df.loc[:, 'width'] = pd.to_numeric(df['width'], errors='coerce')
+df['area'] = df['height'] * df['width']
+print(f"The largest artwork is {df.loc[df['area'].idxmax(), :]}.")
 
 
