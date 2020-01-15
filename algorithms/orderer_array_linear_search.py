206df1f7f155f6fd75744b813aaa6132a9e58945
Paula Onea <paula.onea@gmail.com>
2020-01-15 18:57:25 +0000

    linear search

--- algorithms/orderer_array_linear_search.py
+++ algorithms/orderer_array_linear_search.py
@@ -3,12 +3,12 @@
 
 
 def linear_search(array, number):
-    for i in range(len(array)-1):
+    for i in range(len(array)):
         if array[i] == number:                          # The search stops here, no need to go to the end
             return f'Value {number} was found at position {i+1}.'
     return f'Value {number} was not found in the array.'
 
 
 array = [1, 12, 14, 18, 30]
-value = 18
+value = 1
 print(linear_search(array, value))
