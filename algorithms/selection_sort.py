c39eface9e6e3055f08d7aef1397d6033e5b7219
Paula Onea <paula.onea@gmail.com>
2020-01-20 14:49:54 +0000

    added descending sorting

--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -2,7 +2,7 @@
 # efficiency - O(N^2)
 
 
-def selection_sort(array):
+def selection_sort_asc(array):
     for i in range(len(array)-1):
         index_min = i
         for j in range(i+1, len(array), 1):
@@ -13,6 +13,19 @@ def selection_sort(array):
     return array
 
 
-array = [19, 3, 1, 17, 15, 4]
-array_sorted = selection_sort(array)
-print(array_sorted)
+def selection_sort_dsc(array):
+    for i in range(len(array)-1):
+        index_max = i
+        for j in range(i+1, len(array), 1):
+            if array[j] > array[index_max]:
+                index_max = j
+        if index_max != i:
+            array[i], array[index_max] = array[index_max], array[i]
+    return array
+
+
+array_unsorted = [19, 3, 1, 17, 15, 4]
+array_sorted_asc = selection_sort_asc(array_unsorted)
+print(array_sorted_asc)
+array_sorted_dsc = selection_sort_dsc(array_unsorted)
+print(array_sorted_dsc)
